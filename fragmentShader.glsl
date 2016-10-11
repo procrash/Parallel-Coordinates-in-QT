f4ead425941aef3002170e39068581d9f66d17fc
Wolfgang Meyerle <procrash@neusob.de>
2016-10-11 12:02:52 +0200

    Code cleanup
    introduced OpenGL Index Arrays
    Native Support for OpenGL instead of QTs Onboard Support (this fixed also an issue with fullscreen switch functionality)

--- fragmentShader.glsl
+++ fragmentShader.glsl
@@ -1,7 +1,5 @@
 #version 330
 out vec4 aColor;
 void main() {
-   // gl_FragColor = col;
-   // gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
     aColor = vec4(1.0, 0.0, 0.0, 1.0);
 }
--- vertexShader.glsl
+++ vertexShader.glsl
@@ -1,12 +1,6 @@
 #version 330
 layout(location=0) in vec4 posAttr;
-//attribute highp vec4 posAttr;
-// attribute lowp vec4 colAttr;
-// varying lowp vec4 col;
 uniform highp mat4 matrix;
 void main() {
-   //col = colAttr;
-   // gl_Position = posAttr;
    gl_Position = matrix * vec4(posAttr.xyz, 1.0);
-   //gl_FrontColor = gl_Color;
 }
