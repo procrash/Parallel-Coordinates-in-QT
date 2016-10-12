c90b61fb7ae9fc4cffd1ee25f5e0c469a129b683
Wolfgang Meyerle <procrash@neusob.de>
2016-10-12 07:40:12 +0200

    Save of current work

--- view3d.cpp
+++ view3d.cpp
@@ -117,11 +117,11 @@ void View3D<T>::initializeData()
 
 
     GLfloat vertices[] = {
-              +0.0f, +0.0f,   0.0f,
-              +1.0f, +1.0f,   0.0f,
-              -1.0f, +1.0f,   0.0f,
-              -1.0f, -1.0f,   0.0f,
-              +1.0f, -1.0f,   0.0f
+              +0.0f, +0.0f, 0.0f, /* Color ->*/ 1.0f, 0.0f, 0.0f,
+              +1.0f, +1.0f, 0.0f, /* Color ->*/ 1.0f, 0.0f, 0.0f,
+              -1.0f, +1.0f, 0.0f, /* Color ->*/ 1.0f, 0.0f, 0.0f,
+              -1.0f, -1.0f, 0.0f, /* Color ->*/ 1.0f, 0.0f, 0.0f,
+              +1.0f, -1.0f, 0.0f, /* Color ->*/ 1.0f, 0.0f, 0.0f
            };
 
 
@@ -138,8 +138,10 @@ void View3D<T>::initializeData()
     glBindVertexArray(vertexArrayId);
 
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-    m_program->setAttributeBuffer(0,GL_FLOAT, 0, 0, 0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat)*6, 0);
+
+
+    m_program->setAttributeBuffer(0,GL_FLOAT, 0, 0, 6);
 
     // Create Index Buffer...
     GLushort indices[] = { 0,1,2, 0,3,4 };
